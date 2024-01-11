4f671df7d689a2eadd7d1e489168a98c11d88a81
Richard <richardchoi54@gmail.com>
2024-01-11 13:37:22 -0500

    createQuizzes program accepts user input to customize number of questions, number of studentds, the contents of the question and the answers

--- CreateQuizzes.py
+++ CreateQuizzes.py
@@ -1,7 +1,3 @@
-# Say you’re a geography teacher with 35 students in your class and you want to give a pop quiz on Anime. Alas, your class has a few bad eggs in it, and you can’t trust the students not to cheat. You’d like to randomize the order of questions so that each quiz is unique, making it impossible for anyone to crib answers from anyone else. Of course, doing this by hand would be a lengthy and boring affair. Fortunately, you know some Python.
-
-# 5 students
-
 # Randomize order of questions so all quizzes are unique Y
 
 # 10 multiple choice questions for each quiz, in random order Y
@@ -12,86 +8,93 @@
 
 # Create 35 answer keys for the 35 quizzes Y
 
+# Accept input from user, where the user can add their own customized questions, answers, multiple choices, the number of students and the number of questions. Y
+
+# Take those customized inputs and create quizzes Y
+
+# Save the input regardless of quitting the program to 'save' your progress
+
 import random, os, sys
 from pathlib import Path
 
-questions = [
-    {
-        'q': 'Who is the main character of Kaguya-sama: Love is Not War?',
-        'choices': ['Miyuki Shirogane', 'Kaguya Shinomiya', 'Kei Shirogane', 'Both 1 and 2'],
-        'a': 'Both 1 and 2',
-    },
-       {
-        'q': 'Which of the following is not an isekai anime?',
-        'choices': ['Steins:Gate;','Re:Creators','Gate','Konosuba'],
-        'a': 'Steins:Gate;',
-    },
-       {
-        'q': 'What are the 7 main servant classes in the fate series?',
-        'choices': ['caster, berserker, rider, assassin, saber, archer, lancer',
-        'shielder, alter-ego, foreigner, avenger, pretender, ruler, moon cancer',
-        'caster, berserker, rider, ruler, saber, archer, lancer',
-        'caster, berserker, rider, assassin, foreigner, archer, lancer'],
-        'a': 'caster, berserker, rider, assassin, saber, archer, lancer',
-    },
-       {
-        'q': 'Where does Luffy originally start off from, in regards to the 4 different sections of the world?',
-        'choices': ['West Blue',
-        'North Blue',
-        'East Blue',
-        'South Blue'],
-        'a': 'East Blue',
-    },
-      {
-        'q': 'How are you able to summon a high level monster in the game of Duel Monsters, Yu-Gi-Oh?',
-        'choices': ['Use the power of friendship to overcome any obstacle.',
-        'Use the heart of the cards to guide your way.',
-        'Sacrifice your life points to 1, flip your deck upside down, and dance for 30 seconds.',
-        'Sacrifice at least one of your monsters that you currently control.'],
-        'a': 'Sacrifice at least one of your monsters that you currently control.',
-    },
-      {
-        'q': 'When are you ususally able to challenge the Elite Four?',
-        'choices': ['When you get 16 badges.',
-        'When you get 9 badges.',
-        'When you get 8 badges.',
-        'When you get 3 badges.'],
-        'a': 'When you get 8 badges.',
-    }
-]
-
-#exclude 'a' and exclude 'q' when randomizing your questions
-
-totNumOfStudents = 6
-
-for i in range(1,totNumOfStudents):
+def createQuiz():
     try:
-        currentWorkingPath = Path.cwd()
-        quizPath = Path(str(currentWorkingPath) + rf'\quiz{i}.txt')
-        quizAnswerPath = Path(str(currentWorkingPath) + rf'\quizAnswer{i}.txt')
-
-        quizFile = open(quizPath, 'w')
-        quizAnswerFile = open(quizAnswerPath, 'w')
-
-        # randomize the question order
-        random.shuffle(questions)
-
-        for index,items in enumerate(questions):
-            random.shuffle(items['choices'])
-            quizFile.write(f'''
-                Question {index+1}: {items['q']}
-                1. {items['choices'][0]}
-                2. {items['choices'][1]}
-                3. {items['choices'][2]}
-                4. {items['choices'][3]}
-                ''')
-            quizAnswerFile.write(f'''
-                Question {index+1}: {items['q']}
-                Answer: {items['a']}
-            ''')
-
-            
-        quizFile.close()
         
-    except KeyboardInterrupt:
+        questions = [
+        ]
+
+        print("How many students are in your classroom?")
+        numOfStudents = int(input())
+
+        print("How many questions do you want on your quiz/test?")
+        numOfQuestions = int(input())
+
+        for i in range(1, numOfQuestions+1):
+            exam = {}
+            print(f"What will the question be for question {i}?")
+            question = input()
+
+            exam.setdefault('question', question)
+            choices = []
+            for j in range(1,5):
+                print(f"What will be the multiple choice {j} for this question?") 
+                choice = input()
+    
+                choices.append(choice)
+            exam.setdefault('choices', choices)
+            print(f"What will be the answer for question {i}, '{question}'?") 
+            answer = input()
+            exam.setdefault('answer', answer)
+    
+
+            questions.append(exam)
+
+    # exclude 'a' and exclude 'q' when randomizing your questions
+            currentWorkingPath = Path.cwd()
+
+            if not Path(str(currentWorkingPath) + rf'\quizzes') and not Path(str(currentWorkingPath) + rf'\answers'):
+                os.makedirs(Path(str(currentWorkingPath) + rf'\quizzes'))
+                os.makedirs(Path(str(currentWorkingPath) + rf'\answers'))
+
+        for i in range(1, numOfStudents+1):
+
+            quizPath = Path(str(currentWorkingPath) + rf'\quizzes\quiz{i}.txt')
+            quizAnswerPath = Path(str(currentWorkingPath) + rf'\answers\quizAnswer{i}.txt')
+
+            quizFile = open(quizPath, 'w')
+            quizAnswerFile = open(quizAnswerPath, 'w')
+
+            # randomize the question order
+            random.shuffle(questions)
+
+            for index,items in enumerate(questions):
+                random.shuffle(items['choices'])
+                quizFile.write(f'''
+
+    Question {index+1}: {items['question']}
+        1. {items['choices'][0]}
+        2. {items['choices'][1]}
+        3. {items['choices'][2]}
+        4. {items['choices'][3]}
+
+                ''')
+
+                quizAnswerFile.write(f'''
+
+    Question {index+1}: {items['question']}
+
+        Answer: {items['answer']}
+
+                ''')
+
+
+            quizFile.close()
+            quizAnswerFile.close()
+    except:
+        print("Your input was invalid or something went wrong, please try again!")
         sys.exit()
+
+try:
+    createQuiz()
+except KeyboardInterrupt:
+    sys.exit()
